3d99ca8c4e841240441ccb69979f4be1d567d16a
Rohit Pathak <rohitpathak.3@gmail.com>
2018-12-30 14:37:00 -0500

    Add validation to login form

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -10,6 +10,7 @@ import { Router } from '@angular/router';
 export class LoginComponent implements OnInit {
   userName: string;
   password: string;
+  mouseOverLogin: boolean;
 
   constructor(private authService: AuthService, private router: Router) { }
 
